bd678ef0bd6c98f1ca399fd1d1edf2fd922f3d79
nhistory <sehwan.lee.dev@gmail.com>
2022-11-30 00:49:34 -0400

    Added kijiji cars endpoint test

--- app/endpoints/test_car_routes.py
+++ app/endpoints/test_car_routes.py
@@ -1,13 +1,27 @@
 from fastapi.testclient import TestClient
-from .car_routes import router
+import requests
 
 
-client = TestClient(router)
-
-
-# test
-def test_read_root() -> object:
-  response = client.get("/")
+def test_get_root():
+  response = requests.get("http://127.0.0.1:8000/")
   assert response.status_code == 200
   assert response.json() == {"message": "Hello World"}
+  pass
 
+
+def test_get_cars():
+  response = requests.get("http://127.0.0.1:8000/api/car")
+  response_body = response.json()
+  assert response.status_code == 200
+  assert response.headers["Content-Type"] == "application/json"
+  assert len(response_body) == 500
+  pass
+
+
+def test_get_kijiji_cars():
+  response = requests.get("http://127.0.0.1:8000/api/kijijiCar")
+  response_body = response.json()
+  assert response.status_code == 200
+  assert response.headers["Content-Type"] == "application/json"
+  assert len(response_body) == 40
+  pass
