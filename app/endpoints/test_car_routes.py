a6563b4e6aeffdb97bc80c8bf2fa7c93a3a5ecaf
nhistory <sehwan.lee.dev@gmail.com>
2022-11-29 20:23:58 -0400

    test commit

--- app/endpoints/test_car_routes.py
+++ app/endpoints/test_car_routes.py
@@ -5,7 +5,9 @@ from .car_routes import router
 client = TestClient(router)
 
 
+# test
 def test_read_root() -> object:
   response = client.get("/")
   assert response.status_code == 200
   assert response.json() == {"message": "Hello World"}
+
