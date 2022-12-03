a8a9c4102bf58f5c92cc5eec5e336367d1cebc76
jhchoi0830 <jaehyukchoi1996@gmail.com>
2022-12-03 12:42:11 -0500

    user user_routes

--- app/endpoints/user_routes.py
+++ app/endpoints/user_routes.py
@@ -2,7 +2,42 @@
 from fastapi import FastAPI, HTTPException, APIRouter
 
 # import model
-from app.models.UsedCarModel import UsedCar
-from app.models.UserModel import User
+from app.models.usedCarModel import UsedCar
+from app.models.userModel import User
 
 router = APIRouter()
+
+from app.services.database import (
+    fetch_all_users,
+    fetch_user_by_email,
+    fetch_user_by_password
+    )
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@router.get("/api/car")
+async def get_user() -> list:
+    response = await fetch_all_users()
+    return response
+
+
+@router.get("/register/user/email/{email}", response_model=list[User])
+async def create_email(email: str) -> list:
+    response = await fetch_user_by_email(email)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no user with the email {email}")
+
+@router.get("/register/user/password/{password}", response_model=list[User])
+async def create_password(password: str) -> list:
+    response = await fetch_user_by_password(password)
+    if response:
+        return response
+    raise HTTPException(404, f"The password is incorrect")
+
+
