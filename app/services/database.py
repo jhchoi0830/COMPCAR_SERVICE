2b3506296e90e83734b654aaf533ae7160756373
jhchoi0830 <jaehyukchoi1996@gmail.com>
2022-12-11 19:50:03 -0500

    user route conflict

--- app/services/database.py
+++ app/services/database.py
@@ -50,3 +50,24 @@ async def create_user(user: User):
     result = await user_collection.insert_one(user)
     created_user = await user_collection.find_one({'_id': result.inserted_id})
     return created_user
+
+async def fetch_car_by_year(year: int) -> list:
+    cars = []
+    cursor = car_collection.find({"madeYear":year})
+    async for document in cursor:
+        cars.append(document)
+    return cars
+
+async def fetch_car_by_mileage(id: int) -> list:
+    cars = []
+    cursor = car_collection.find({"mileage": {'$gt':(id-1)*50000,'$lt':id*50000}})
+    async for document in cursor:
+        cars.append(document)
+    return cars
+
+async def fetch_car_by_price(id: int) -> list:
+    cars = []
+    cursor = car_collection.find({"price.price": {'$gt':(id-1)*10000,'$lt':id*10000} })
+    async for document in cursor:
+        cars.append(document)
+    return cars
